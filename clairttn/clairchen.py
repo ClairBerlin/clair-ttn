3fb4eeb8bdada4c063a34a05d3294a7cd68a7538
Jan Weil <jan.weil@web.de>
2020-10-13 11:29:00 +0200

    Add draft for JSON-API posts

--- clairttn/clairchen.py
+++ clairttn/clairchen.py
@@ -1,5 +1,10 @@
 from clairttn.types import *
 
+
 class ClairchenDeviceUUID(DeviceUUID):
     def __init__(self, device_id: bytes):
         super().__init__(device_id, "CLAIRCHEN")
+
+
+def decode_payload(payload, rx_datetime, mcs):
+    return []
--- clairttn/handler.py
+++ clairttn/handler.py
@@ -3,7 +3,9 @@ import logging
 import base64
 import dateutil.parser as dtparser
 import datetime as dt
+import jsonapi_requests as jarequests
 import clairttn.ers as ers
+import clairttn.clairchen as clairchen
 
 
 class _Handler:
@@ -20,7 +22,10 @@ class _Handler:
             payload = base64.b64decode(message.payload_raw)
             logging.debug("decoded payload: {}".format(payload.hex('-').upper()))
 
-            self._handle_message(payload, message)
+            device_id = bytes.fromhex(message.hardware_serial)
+            logging.debug("device_id: {}".format(device_id.hex()))
+
+            self._handle_message(payload, device_id, message)
 
         self._mqtt_client.set_uplink_callback(_uplink_callback)
 
@@ -30,41 +35,89 @@ class _Handler:
     def close(self):
         self._mqtt_client.close()
 
-    def _handle_message(self, payload, message):
+    def _handle_message(self, payload, device_id, message):
         raise NotImplementedError("needs to be implemented by subclass")
 
 
 class _SampleForwardingHandler(_Handler):
-    def _handle_message(self, payload, message):
+    def __init__(self, app_id, access_key):
+        super().__init__(app_id, access_key)
+        api = jarequests.Api.config({
+            'API_ROOT': 'http://localhost:8888/api/data/v1/'
+        })
+        self._sample_endpoint = api.endpoint('ingest')
+
+    def _handle_message(self, payload, device_id, message):
         rx_datetime = dtparser.parse(message.metadata.time)
         logging.debug("rx_datetime: {}".format(rx_datetime))
-        samples = self._decode_payload(payload, rx_datetime)
-        for sample in samples:
-            logging.debug(sample)
-            logging.debug(dt.datetime.fromtimestamp(sample.timestamp))
-            logging.debug(sample.co2)
-            logging.debug(sample.temperature)
-            logging.debug(sample.relative_humidity)
 
-    def _decode_payload(self, payload, rx_datetime):
+        device_uuid = self._uuid_class(device_id)
+        logging.debug("device_uuid: {}".format(device_uuid))
+
+        samples = self._decode_payload(payload, rx_datetime, message)
+        for sample in samples:
+            self._post_sample(sample, device_uuid)
+
+    def _decode_payload(self, payload, rx_datetime, message):
         raise NotImplementedError("needs to be implemented by subclass")
+
+    def _post_sample(self, sample, device_uuid):
+        logging.debug("timestamp: {}".format(dt.datetime.fromtimestamp(sample.timestamp)))
+        logging.debug("co2: {}".format(sample.co2))
+        logging.debug("temperature: {}".format(sample.temperature))
+        logging.debug("humidity: {}".format(sample.relative_humidity))
+
+        sample_attributes = {
+            "timestamp_s": sample.timestamp,
+            "co2_ppm": sample.co2.value
+        }
+        if sample.temperature:
+            sample_attributes["temperature_celsius"] = sample.temperature.value
+        if sample.relative_humidity:
+            sample_attributes["rel_humidity_percent"] = sampe.relative_humidity.value
+
+        sample_object = jarequests.JsonApiObject(
+            type = 'Sample',
+            attributes = sample_attributes,
+            relationships = {
+                "node_ref": {
+                    "data": {
+                        "type": "Node",
+                        "id": str(device_uuid)
+                    }
+                }
+            }
+        )
+
+        response = self._sample_endpoint.post(object=sample_object)
+        logging.debug("response: {}".format(response))
 
 
 class ClairchenForwardingHandler(_SampleForwardingHandler):
-    pass
+    def __init__(self, app_id, access_key):
+        super().__init__(app_id, access_key)
+        self._uuid_class = clairchen.ClairchenDeviceUUID
+
+    def _decode_payload(self, payload, rx_datetime, message):
+        mcs = LoRaWanMcs[message.metadata.data_rate]
+        return clairchen.decode_payload(payload, rx_datetime, mcs)
 
 
 class ErsForwardingHandler(_SampleForwardingHandler):
-    def _decode_payload(self, payload, rx_datetime):
+    def __init__(self, app_id, access_key):
+        super().__init__(app_id, access_key)
+        self._uuid_class = ers.ErsDeviceUUID
+
+    def _decode_payload(self, payload, rx_datetime, message):
         return ers.decode_payload(payload, rx_datetime)
 
 
 class ErsConfigurationHandler(_Handler):
-    def _handle_message(self, payload, message):
+    def _handle_message(self, payload, device_id, message):
         logging.debug("uplink message received from {}".format(message.dev_id))
         logging.debug(str(message))
 
-        if not self._is_conforming(payload):
+        if not self._is_conforming(payload, message):
             logging.debug("message is not conforming to protocol payload specification")
 
             mcs = LoRaWanMcs[message.metadata.data_rate]
@@ -81,7 +134,7 @@ class ErsConfigurationHandler(_Handler):
 
             self._mqtt_client.send(message.dev_id, b64_payload, port, conf=False)
 
-    def _is_conforming(self, payload):
+    def _is_conforming(self, payload, message):
         measurement_count = len(ers.decode_payload(payload))
         logging.debug("measurement count: {}".format(measurement_count))
 
