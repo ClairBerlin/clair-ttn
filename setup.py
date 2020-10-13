69c71b606f2ef29d540f2a4b9cedf5a95184f4d3
Jan Weil <jan.weil@web.de>
2020-10-13 13:04:05 +0200

    Add jsonapi_requests dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     include_package_data=True,
     install_requires=[
         'Click',
-        'ttn'
+        'ttn',
+        'jsonapi_requests'
     ],
     entry_points='''
     [console_scripts]
