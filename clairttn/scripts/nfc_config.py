9bb7a2ee7fdd623138b918d1195dc7c5091b589a
Jan Weil <jan.weil@web.de>
2020-12-07 12:17:30 +0100

    Add device registration tools
    * clair-register-device-in-ttn
    * clair-register-device-in-managair
    * clair-generate-nfc-config
    
    Rename fixture tool to
    
    * clair-generate-fixtures-from-backup

---
+++ clairttn/scripts/nfc_config.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import click
+import ttn
+import sys
+import clairttn.ers as ers
+import pyqrcode
+
+
+def _get_ttn_device(ttn_app_id, access_key, device_eui):
+    device_id = ers.ErsDeviceUUID(bytes.fromhex(device_eui))
+    applicationClient = ttn.HandlerClient(ttn_app_id, access_key).application()
+    return applicationClient.device(str(device_id))
+
+
+def _generate_nfc_config(ttn_device):
+    app_eui = ttn_device.lorawan_device.app_eui.hex()
+    app_key = ttn_device.lorawan_device.app_key.hex()
+
+    nfc_config = """\
+AppEui:{}
+Ota:true
+Ack:false
+AppKey:{}
+SplPer:356
+Co2Per:1
+TempPer:0
+SendPer:3
+QSize:5
+QOffset:false
+QPurge:true""".format(app_eui, app_key)
+
+    return nfc_config
+
+
+def _generate_qr_png(nfc_config, fila_name):
+    config_code = pyqrcode.create(nfc_config)
+    config_code.png(fila_name, scale=4)
+
+
+@click.command()
+@click.option('-i', '--ttn-app-id', required=True)
+@click.option('-k', '--access-key-file', required=True, type=click.File())
+@click.argument("device-eui")
+def generate_nfc_config(ttn_app_id, access_key_file, device_eui):
+    """Create NFC config files for a device registered in the TTN.
+
+    \b
+    DEVICE_EUI is the TTN device EUI.
+    """
+    access_key = access_key_file.read().rstrip('\n')
+
+    try:
+        ttn_device = _get_ttn_device(ttn_app_id, access_key, device_eui)
+    except RuntimeError:
+        print("device {} not found".format(device_eui))
+        sys.exit(1)
+
+    nfc_config = _generate_nfc_config(ttn_device)
+
+    _generate_qr_png(nfc_config, "{}-nfc-config.png".format(device_eui))
---
+++ clairttn/scripts/register_device.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+import click
+import ttn
+import clairttn.ers as ers
+import os
+import sys
+import binascii
+import requests
+import jsonapi_requests as jarequests
+import json
+import getpass
+
+
+def _create_ttn_device(device_eui, app_eui):
+    application_key = binascii.b2a_hex(os.urandom(16)).upper()
+    ttn_device = {
+        "appEui": app_eui,
+        "devEui": device_eui,
+        "appKey": application_key
+    }
+    return ttn_device
+
+
+@click.command()
+@click.option('-i', '--ttn-app-id', required=True)
+@click.option('-k', '--access-key-file', required=True, type=click.File())
+@click.argument("device-eui")
+@click.argument("app-eui")
+def register_device_in_ttn(ttn_app_id, access_key_file, device_eui, app_eui):
+    """Register a Clair TTN node in the TTN.
+
+    \b
+    DEVICE_EUI is the TTN device EUI.
+    APP_EUI is the TTN app EUI.
+    """
+
+    access_key = access_key_file.read().rstrip('\n')
+    applicationClient = ttn.HandlerClient(ttn_app_id, access_key).application()
+
+    device_id = str(ers.ErsDeviceUUID(bytes.fromhex(device_eui)))
+    ttn_device = _create_ttn_device(device_eui, app_eui)
+
+    applicationClient.register_device(device_id, ttn_device)
+
+
+class _TokenAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers['Authorization'] = "Token {}".format(self.token)
+        return r
+
+
+def _login(api_root):
+    login_endpoint = jarequests.Api.config({ 'API_ROOT': api_root }).endpoint('auth/login')
+
+    username = input("Username: ")
+    password = getpass.getpass()
+
+    login_object = jarequests.JsonApiObject(
+        type = 'LoginView',
+        attributes = {
+            "username": username,
+            "password": password
+        })
+
+    try:
+        r = login_endpoint.post(object=login_object)
+    except jarequests.request_factory.ApiClientError as e:
+        error_object = json.loads(e.content)
+        print("login failed: {}".format(error_object['errors'][0]['detail']))
+        sys.exit(1)
+
+    return r.data.attributes['key']
+
+
+def _create_node(api_root, device_id, ttn_device, protocol_id, model_id, owner_id):
+    key = _login(api_root)
+
+    api = jarequests.Api.config({
+        'API_ROOT': api_root,
+        'AUTH': _TokenAuth(key)
+    })
+
+    nodes_endpoint = api.endpoint('nodes')
+
+    node_object = jarequests.JsonApiObject(
+        type = 'Node',
+        id = device_id,
+        attributes = {
+            "eui64": ttn_device["devEui"],
+            "alias": ttn_device["devEui"]
+        },
+        relationships = {
+            "protocol": { "data": { "type": "Protocol", "id": protocol_id } },
+            "model": { "data": { "type": "Model", "id": model_id } },
+            "owner": { "data": { "type": "Organization", "id": owner_id } }
+        }
+    )
+
+    response = nodes_endpoint.post(object=node_object)
+
+
+@click.command()
+@click.option('-r', '--api-root', default='http://localhost:8888/api/v1/', show_default=True)
+@click.argument("device-eui")
+@click.argument("protocol-id")
+@click.argument("model-id")
+@click.argument("owner-id")
+def register_device_in_managair(api_root, device_eui, protocol_id, model_id, owner_id):
+    """Register a Clair TTN node in the managair.
+
+    \b
+    DEVICE_EUI is the TTN device EUI.
+    PROTOCOL_ID is the id of the (existing) node protocol in the managair.
+    MODEL_ID is the id of the (existing) node model in the managair.
+    OWNER_ID is the id of the (existing) owner organization in the managair.
+
+    You will be prompted to enter usename and password of an account which
+    needs to be a member of the owner organization. The account needs to have
+    the right to create a node.
+    """
+    device_id = str(ers.ErsDeviceUUID(bytes.fromhex(device_eui)))
+
+    _create_node(api_root, device_id, ttn_device, protocol_id, model_id, owner_id)
