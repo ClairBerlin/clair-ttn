732fbc52669bf2ea51792ac252e5efa870693162
Jan Weil <jan.weil@web.de>
2021-09-20 14:42:57 +0200

    Add good case test for TTN v3

--- tests/test_ttn_handler.py
+++ tests/test_ttn_handler.py
@@ -1,8 +1,88 @@
 import clairttn.ttn_handler as ttn_handler
 import pytest
+import base64
+import dateutil.parser as dtparser
 
 
 class TestTTNMessageDecoding:
+    def testRegularUplinkMessage(self):
+        app_id = "dummy"
+        access_key = "dummy"
+        v3_handler = ttn_handler.TtnV3Handler(app_id, access_key)
+
+        regular_message = {
+            "end_device_ids": {
+                "device_id": "clairfeatherprotored",
+                "application_ids": {"application_id": "clairchen-test"},
+                "dev_eui": "9876B600001193E0",
+                "join_eui": "70B3D57ED00347BA",
+                "dev_addr": "260BC3D6",
+            },
+            "correlation_ids": [
+                "as:up:01FG1JPY8VGZWRTHPJGY4Y41VM",
+                "ns:uplink:01FG1JPY2DYSQ25A54MTNHWRXZ",
+                "pba:conn:up:01FG16560GWJ3GWMXXXEH02WCH",
+                "pba:uplink:01FG1JPY2AJ2PQ0YCFKYT6ZGXF",
+                "rpc:/ttn.lorawan.v3.GsNs/HandleUplink:01FG1JPY2DSWV5XCSEJ1M92T4J",
+                "rpc:/ttn.lorawan.v3.NsAs/HandleUplink:01FG1JPY8VY8M6M3FH1RH5MJQ0",
+            ],
+            "received_at": "2021-09-20T12:25:53.180595270Z",
+            "uplink_message": {
+                "session_key_id": "AXuyqD7znKml0qbj/8Y0QQ==",
+                "f_port": 1,
+                "f_cnt": 2621,
+                "frm_payload": "Aie0KLQotA==",
+                "rx_metadata": [
+                    {
+                        "gateway_ids": {"gateway_id": "packetbroker"},
+                        "packet_broker": {
+                            "message_id": "01FG1JPY2AJ2PQ0YCFKYT6ZGXF",
+                            "forwarder_net_id": "000013",
+                            "forwarder_tenant_id": "ttnv2",
+                            "forwarder_cluster_id": "ttn-v2-eu-3",
+                            "forwarder_gateway_eui": "C0EE40FFFF293507",
+                            "forwarder_gateway_id": "eui-c0ee40ffff293507",
+                            "home_network_net_id": "000013",
+                            "home_network_tenant_id": "ttn",
+                            "home_network_cluster_id": "ttn-eu1",
+                        },
+                        "rssi": -121,
+                        "channel_rssi": -121,
+                        "snr": -10.2,
+                        "location": {
+                            "latitude": 52.48453486,
+                            "longitude": 13.34653402,
+                            "altitude": 46,
+                        },
+                        "uplink_token": "eyJnIjoiWlhsS2FHSkhZMmxQYVVwQ1RWUkpORkl3VGs1VE1XTnBURU5LYkdKdFRXbFBhVXBDVFZSSk5GSXdUazVKYVhkcFlWaFphVTlwU1RCU1Z6RXdaVzEwWVZOc2FIWlhWRVpHVVd0d05FbHBkMmxrUjBadVNXcHZhVlJJU2paaWJFSnhZV3hrTlUxc1pGWlJNMEpWVDFWS1JWUXlaRUprZVVvNUxtTjVWbGt6Y3pocmJGZFdja1pYTjJsVGJXUnZkMmN1T1VocmNYRjVWbk5ZVVZsTVh6SkNXaTVOV1dGblQwWkRUMkpUU1VodWQwTkZjMUZ3VUZseVprNVVXRlZvVVZSWlJVeFhVM3BoVWswM01qZEhWa016ZEhOdWJqVnhVV2RSVlVKRk5WUlFTVWR1VkdwbFJHMVVTVEJhVUVFelRWVmFkV0kxU1VWUE5qaHJSemM0ZDJsSVNEQTRXRTVvVTNoa01VaFBVako1ZVY5cFNFOUZOMjFGTW5BNVEzcHlXa1owZDNscVdTMXhUamhXUTE5aU1rYzNVVE5FVEMxVk1XOHhhazFYTFRkVlZsOU1aVGswY0VGSlpFbFNVVWRHVVRSbkxsQjJORXRCT0RJMlpFOUNXSFpzU0hFNFUyRlhZVkU9IiwiYSI6eyJmbmlkIjoiMDAwMDEzIiwiZnRpZCI6InR0bnYyIiwiZmNpZCI6InR0bi12Mi1ldS0zIn19",
+                    }
+                ],
+                "settings": {
+                    "data_rate": {"lora": {"bandwidth": 125000, "spreading_factor": 9}},
+                    "data_rate_index": 3,
+                    "coding_rate": "4/5",
+                    "frequency": "867100000",
+                },
+                "received_at": "2021-09-20T12:25:52.973476075Z",
+                "consumed_airtime": "0.185344s",
+                "network_ids": {
+                    "net_id": "000013",
+                    "tenant_id": "ttn",
+                    "cluster_id": "ttn-eu1",
+                },
+            },
+        }
+
+        rx_message = v3_handler._extract_rx_message(regular_message)
+
+        assert rx_message.device_eui == bytearray.fromhex("9876B600001193E0")
+        assert rx_message.device_id == "clairfeatherprotored"
+        assert rx_message.raw_data == base64.b64decode("Aie0KLQotA==")
+        assert rx_message.rx_datetime == dtparser.parse(
+            "2021-09-20T12:25:52.973476075Z"
+        )
+        assert rx_message.rx_port == 1
+
     def testV3MessageWithoutPayload(self):
         app_id = "dummy"
         access_key = "dummy"
