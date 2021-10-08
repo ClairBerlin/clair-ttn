e7d0e2f835cdea01c70d1d21095410581328444c
Jan Weil <jan.weil@web.de>
2021-10-08 11:04:09 +0200

    Check for background thread in /proc file system

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-MARKER_FILE="/tmp/clair-alive"
-if [ -s $MARKER_FILE ]
-then
-    echo "Background thread was alive.";
-    rm $MARKER_FILE
+
+# exit in case pidof exits with a non-zero status
+set -e
+
+PIDOF_PYTHON=`pidof python`
+NROF_PYTHON_THREADS=`ls -1 /proc/$PIDOF_PYTHON/task | wc -l`
+
+if [ "$NROF_PYTHON_THREADS" = 2 ]; then
+    echo "Background thread was alive."
     exit 0
 else
-    echo "Background thread seems to be dead.";
+    echo "Background thread seems to be dead."
     exit 1
 fi
